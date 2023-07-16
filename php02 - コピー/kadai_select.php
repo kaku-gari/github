1797e7c8392e9e7a72174495cc5ba0a7abbe7497
takahiro <122219910+thkt2008@users.noreply.github.com>
2023-07-16 22:51:27 +0900

    kadai php02

---
+++ php02 - コピー/kadai_select.php
@@ -0,0 +1,68 @@
+<?php
+//1.  DB接続します
+try {
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=purplerhino4_gs_db_book;charset=utf8;host=mysql57.purplerhino4.sakura.ne.jp','purplerhino4','password');
+  // $pdo = new PDO('mysql:dbname=gs_db_book;charset=utf8;host=localhost','root','');
+
+} catch (PDOException $e) {
+  exit('DB Connection Error'.$e->getMessage());
+}
+
+//２．データ登録SQL作成
+$sql = "SELECT * FROM gs_bm_table;";
+$stmt = $pdo->prepare($sql);
+$status = $stmt->execute();
+
+//３．データ表示
+$view="";
+if($status==false) {
+    //execute（SQL実行時にエラーがある場合）
+  $error = $stmt->errorInfo();
+  exit("SQL_Error:".$error[2]);
+
+}else{
+  //Selectデータの数だけ自動でループしてくれる
+  //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
+  while( $res = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $view .= "<p>".$res['id'].",".$res['name']."</p>";
+    $view .= "<p>".$res['url']."</p>";
+    //.=結合演算子。appendみたいなもの
+  }
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>フリーアンケート表示</title>
+<link rel="stylesheet" href="css/range.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+      <a class="navbar-brand" href="kadai_index.php">データ登録</a>
+      </div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<div>
+    <div class="container jumbotron"><?=$view?></div>
+</div>
+<!-- Main[End] -->
+
+</body>
+</html>
